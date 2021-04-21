d80f0941b05d974ed24ac21aa1c36f43dfad14cc
Kevin Spoth <kspoth08@su.edu>
2021-04-21 19:52:49 -0400

    adding header and search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <h1 className="brand">
+          <i className="fas fa-coins"></i>Crypto Tracker
+        </h1>
+        <form>
+          <input
+            className="inputField"
+            type="text"
+            placeholder="Search Currency"
+          ></input>
+        </form>
+      </div>
     </div>
   );
 }
