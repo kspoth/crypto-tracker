dd8e396061d9e11c163a8bbc0a95e6d075c20e59
Kevin Spoth <kspoth08@su.edu>
2021-05-05 19:02:36 -0400

    heroku

---
+++ client/build/precache-manifest.0dbd2ee2b194af70b5855d4924a09b7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d631b2f96d6e7cbab74b72d584729c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7532fae8e2c8a50df0ee",
+    "url": "/static/css/main.4ba41ec4.chunk.css"
+  },
+  {
+    "revision": "20b20e6937cc213526ba",
+    "url": "/static/js/2.2d8bd8da.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2d8bd8da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7532fae8e2c8a50df0ee",
+    "url": "/static/js/main.0e911f7d.chunk.js"
+  },
+  {
+    "revision": "9dd6a93b70f2e0c8e403",
+    "url": "/static/js/runtime-main.88672a9d.js"
+  }
+]);
